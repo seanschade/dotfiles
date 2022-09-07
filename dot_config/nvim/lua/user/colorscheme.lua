79b0ce8c9cdb18daa6b02b7f55af2ddc624d3b7c
Sean Schade <sean.schade@gmail.com>
2022-09-06 19:41:52 -0500

    add nvim config

---
+++ dot_config/nvim/lua/user/colorscheme.lua
@@ -0,0 +1,9 @@
+local colorscheme = "tokyonight"
+
+vim.g.tokyonight_transparent = "true"
+vim.g.tokyonight_transparent_sidebar = "true"
+
+local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
+if not status_ok then
+  return
+end
